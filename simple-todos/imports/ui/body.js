404e3dca1af4cc48cc83432954951fcce5f2be1d
Daniel-Gallup <drust328@millardps.org>
2016-10-15 07:01:17 -0500

    Checkpoint #3

--- simple-todos/imports/ui/body.js
+++ simple-todos/imports/ui/body.js
@@ -1,12 +1,23 @@
 import { Template } from 'meteor/templating';
 import { Tasks } from '../api/tasks.js';
+import { ReactiveDict } from 'meteor/reactive-dict';
 import './task.js';
 import './body.html';
 
+Template.body.onCreated(function bodyOnCreated()
+{
+  this.state = new ReactiveDict();
+});
+
 Template.body.helpers({
-  tasks()
-  {
-    return Tasks.find({}, { sort: { createdAt: -1 } });
+    tasks()
+    {
+	const instance = Template.instance();
+
+	if (instance.state.get('hideCompleted'))
+	    return Tasks.find({ checked: { $ne: true } }, { sort: { createdAt: -1 } });
+	
+	return Tasks.find({}, { sort: { createdAt: -1 } });
   },
 });
 
@@ -25,5 +36,9 @@ Template.body.events({
     });
 
     target.text.value = '';	
-  },
+    },
+    'change .hide-completed input'(event, instance)
+    {
+    instance.state.set('hideCompleted', event.target.checked);
+    },
 });
